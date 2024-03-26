03c175ecbf9622b1f39df69a4cca8601a099462e
LornaPrecious <113431015+LornaPrecious@users.noreply.github.com>
2024-03-26 12:49:16 +0300

    Update settings.py

--- quizapp/settings.py
+++ quizapp/settings.py
@@ -53,8 +53,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'users.apps.UsersConfig',
-    'quiz.apps.QuizConfig',
+    'users',
+    'quiz',
 ]
 
 MIDDLEWARE = [
